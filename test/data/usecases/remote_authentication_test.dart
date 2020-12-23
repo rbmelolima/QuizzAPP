642168869107a4e33b39eaf155921e8153fa54ce
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-23 17:55:13 -0300

    move production code to their own target

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,42 +1,11 @@
 import 'package:faker/faker.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
+import 'package:quizzApp/data/http/http.dart';
+import 'package:quizzApp/data/usecases/usecases.dart';
 import 'package:quizzApp/domain/usecases/usecases.dart';
 import 'package:test/test.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({
-    @required this.httpClient,
-    @required this.url,
-  });
-
-  Future<void> auth(AuthenticationParams params) async {
-    final body = {
-      'email': params.email,
-      'password': params.secret,
-    };
-
-    await httpClient.request(
-      url: url,
-      method: 'post',
-      body: body,
-    );
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request({
-    @required String url,
-    @required String method,
-    Map body,
-  });
-}
-
 class HttpClientSpy extends Mock implements HttpClient {}
-
 void main() {
   RemoteAuthentication sut;
   HttpClientSpy httpClient;
