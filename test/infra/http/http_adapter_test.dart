8427c1d9b6290b6d4abb13c4b4a850c913aa834c
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-24 16:49:03 -0300

    ensure HttpAdapter throws a ServerError with invalid method is provided

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -17,6 +17,15 @@ void main() {
     sut = HttpAdapter(client);
     url = faker.internet.httpUrl();
   });
+
+  group('shared', () {
+    test('Should throw ServerError if invalid method is provided', () async {
+      final future = sut.request(url: url, method: 'invalid_method');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
+
   group('post', () {
     PostExpectation mockRequest() => when(
         client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
@@ -92,19 +101,19 @@ void main() {
       mockResponse(401);
       final future = sut.request(url: url, method: 'post');
       expect(future, throwsA(HttpError.unauthorized));
-    });  
-    
+    });
+
     test('Should return ForbidenError if post returns 403', () async {
       mockResponse(403);
       final future = sut.request(url: url, method: 'post');
       expect(future, throwsA(HttpError.forbidden));
-    }); 
+    });
 
     test('Should return NotFoundError if post returns 404', () async {
       mockResponse(404);
       final future = sut.request(url: url, method: 'post');
       expect(future, throwsA(HttpError.notFound));
-    });  
+    });
 
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
