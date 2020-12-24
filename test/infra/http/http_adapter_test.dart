326d39c9ba42230c2d6e8de2b5f675da23100007
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-24 16:06:45 -0300

    ensure HttpAdapter returns null if post returns 204 with data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -30,7 +30,12 @@ class HttpAdapter implements HttpClient {
       body: jsonBody,
     );
 
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    }
+    else {
+      return null;
+    }
   }
 }
 
@@ -96,6 +101,14 @@ void main() {
     });
 
     test('Should return null if post returns 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
       mockResponse(204, body: '');
 
       final response = await sut.request(url: url, method: 'post');
