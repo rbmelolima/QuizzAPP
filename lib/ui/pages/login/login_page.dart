e54e1253da8fce4e6dac4274907c50db747f516d
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-26 18:51:33 -0300

    ensure LoginPage presents error if password is invalid

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -23,27 +23,36 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   children: [
                     StreamBuilder<String>(
-                        stream: presenter.emailErrorStream,
-                        builder: (context, snapshot) {
-                          return TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              errorText: snapshot.data?.isEmpty == true
-                                  ? null
-                                  : snapshot.data,
-                            ),
-                            keyboardType: TextInputType.emailAddress,
-                            onChanged: presenter.validateEmail,
-                          );
-                        }),
+                      stream: presenter.emailErrorStream,
+                      builder: (context, snapshot) {
+                        return TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            errorText: snapshot.data?.isEmpty == true
+                                ? null
+                                : snapshot.data,
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: presenter.validateEmail,
+                        );
+                      },
+                    ),
                     Container(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Senha',
-                      ),
-                      obscureText: true,
-                      keyboardType: TextInputType.emailAddress,
-                      onChanged: presenter.validatePassword,
+                    StreamBuilder<String>(
+                      stream: presenter.passwordErrorStream,
+                      builder: (context, snapshot) {
+                        return TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                            errorText: snapshot.data?.isEmpty == true
+                                ? null
+                                : snapshot.data,
+                          ),
+                          obscureText: true,
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: presenter.validatePassword,
+                        );
+                      },
                     ),
                     Container(height: 16),
                     SizedBox(
--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -11,13 +11,17 @@ class LoginPresenterSpy extends Mock implements LoginPresenter {}
 void main() {
   LoginPresenter presenter;
   StreamController<String> emailErrorController;
+  StreamController<String> passwordErrorController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
     emailErrorController = StreamController<String>();
+    passwordErrorController = StreamController<String>();
 
     when(presenter.emailErrorStream)
         .thenAnswer((_) => emailErrorController.stream);
+    when(presenter.passwordErrorStream)
+        .thenAnswer((_) => passwordErrorController.stream);
 
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
@@ -25,6 +29,7 @@ void main() {
 
   tearDown(() {
     emailErrorController.close();
+    passwordErrorController.close();
   });
 
   testWidgets('Should load with correct initial state',
@@ -109,5 +114,16 @@ void main() {
           of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
       findsOneWidget,
     );
+  });
+
+  testWidgets('Should present error if password is invalid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add('Any error');
+    //Force reloads
+    await tester.pump();
+
+    expect(find.text('Any error'), findsOneWidget);
   });
 }
