c8174db6ddf25e746d39ba3b334072d802c135f6
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-28 18:30:41 -0300

    move EmailInput to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quizzApp/ui/pages/pages.dart';
 import '../../components/components.dart';
+import 'components/components.dart';
 
 class LoginPage extends StatefulWidget {
   final LoginPresenter presenter;
@@ -46,63 +48,52 @@ class _LoginPageState extends State<LoginPage> {
                 Container(height: 24),
                 Padding(
                   padding: const EdgeInsets.all(12.0),
-                  child: Form(
-                    child: Column(
-                      children: [
-                        StreamBuilder<String>(
-                          stream: widget.presenter.emailErrorStream,
-                          builder: (context, snapshot) {
-                            return TextField(
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                errorText: snapshot.data?.isEmpty == true
-                                    ? null
-                                    : snapshot.data,
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                              onChanged: widget.presenter.validateEmail,
-                            );
-                          },
-                        ),
-                        Container(height: 16),
-                        StreamBuilder<String>(
-                          stream: widget.presenter.passwordErrorStream,
-                          builder: (context, snapshot) {
-                            return TextField(
-                              decoration: InputDecoration(
-                                labelText: 'Senha',
-                                errorText: snapshot.data?.isEmpty == true
-                                    ? null
-                                    : snapshot.data,
-                              ),
-                              obscureText: true,
-                              keyboardType: TextInputType.emailAddress,
-                              onChanged: widget.presenter.validatePassword,
-                            );
-                          },
-                        ),
-                        Container(height: 16),
-                        SizedBox(
-                          width: double.maxFinite,
-                          child: StreamBuilder<bool>(
-                            stream: widget.presenter.isFormValidStream,
+                  child: Provider(
+                    create: (_) => widget.presenter,
+                    child: Form(
+                      child: Column(
+                        children: [
+                          EmailInput(),
+                          Container(height: 16),
+                          StreamBuilder<String>(
+                            stream: widget.presenter.passwordErrorStream,
                             builder: (context, snapshot) {
-                              return RaisedButton(
-                                onPressed: snapshot.data == true
-                                    ? widget.presenter.auth
-                                    : null,
-                                child: Text('Entrar'.toUpperCase()),
+                              return TextField(
+                                decoration: InputDecoration(
+                                  labelText: 'Senha',
+                                  errorText: snapshot.data?.isEmpty == true
+                                      ? null
+                                      : snapshot.data,
+                                ),
+                                obscureText: true,
+                                keyboardType: TextInputType.emailAddress,
+                                onChanged: widget.presenter.validatePassword,
                               );
                             },
                           ),
-                        ),
-                        Container(height: 16),
-                        FlatButton.icon(
-                          onPressed: () {},
-                          icon: Icon(Icons.person),
-                          label: Text('Criar conta'.toUpperCase()),
-                        ),
-                      ],
+                          Container(height: 16),
+                          SizedBox(
+                            width: double.maxFinite,
+                            child: StreamBuilder<bool>(
+                              stream: widget.presenter.isFormValidStream,
+                              builder: (context, snapshot) {
+                                return RaisedButton(
+                                  onPressed: snapshot.data == true
+                                      ? widget.presenter.auth
+                                      : null,
+                                  child: Text('Entrar'.toUpperCase()),
+                                );
+                              },
+                            ),
+                          ),
+                          Container(height: 16),
+                          FlatButton.icon(
+                            onPressed: () {},
+                            icon: Icon(Icons.person),
+                            label: Text('Criar conta'.toUpperCase()),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
