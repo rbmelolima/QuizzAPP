204ee6bad25282217b72b593a8dd82a219b66af5
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2021-01-04 17:30:30 -0300

    ensure StreamLoginPresenter emits form valid event if form is valid

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -4,10 +4,17 @@ import 'dart:async';
 import 'package:quizzApp/presentation/protocols/protocols.dart';
 
 class LoginState {
+  String email;
+  String password;
+
   String emailError;
   String passwordError;
 
-  bool get isFormValid => false;
+  bool get isFormValid =>
+      emailError == null &&
+      passwordError == null &&
+      email != null &&
+      password != null;
 }
 
 class StreamLoginPresenter {
@@ -30,11 +37,13 @@ class StreamLoginPresenter {
   void update() => _controller.add(_state);
 
   void validateEmail(String email) {
+    _state.email = email;
     _state.emailError = validation.validate(field: 'email', value: email);
     update();
   }
 
   void validatePassword(String password) {
+    _state.password = password;
     _state.passwordError =
         validation.validate(field: 'password', value: password);
     update();
