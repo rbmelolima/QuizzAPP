de0cd17f4c980dc0a6413cca05f9d56d355de564
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2021-01-08 14:54:57 -0300

    move sut creation to the setup method

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -30,17 +30,20 @@ abstract class SaveSecureCacheStorage {
   });
 }
 
-class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {
-}
+class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {}
 
 void main() {
-  test('Should call SaveSecureCacheStorage with correct values', () async {
-    final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
-    final sut = LocalSaveCurrentAccount(
-      saveSecureCacheStorage: saveSecureCacheStorage,
-    );
-    final account = AccountEntity(faker.guid.guid());
+  LocalSaveCurrentAccount sut;
+  SaveSecureCacheStorageSpy saveSecureCacheStorage;
+  AccountEntity account;
 
+  setUp(() {
+    saveSecureCacheStorage = SaveSecureCacheStorageSpy();
+    sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    account = AccountEntity(faker.guid.guid());
+  });
+
+  test('Should call SaveSecureCacheStorage with correct values', () async {
     await sut.save(account);
 
     verify(saveSecureCacheStorage.saveSecure(
@@ -49,16 +52,7 @@ void main() {
     ));
   });
 
-  test('Should throw UnexpectedError if SaveSecureCacheStorage throws',
-      () async {
-    final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
-
-    final sut = LocalSaveCurrentAccount(
-      saveSecureCacheStorage: saveSecureCacheStorage,
-    );
-
-    final account = AccountEntity(faker.guid.guid());
-
+  test('Should throw UnexpectedError if SaveSecureCacheStorage throws', () async {
     when(saveSecureCacheStorage.saveSecure(
       key: anyNamed('key'),
       value: anyNamed('value'),
