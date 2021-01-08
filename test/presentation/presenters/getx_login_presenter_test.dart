f39a9794585a74510c357910e10a4b25501de89f
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2021-01-08 17:38:09 -0300

    remove hide loading in success case

--- test/presentation/presenters/getx_login_presenter_test.dart
+++ test/presentation/presenters/getx_login_presenter_test.dart
@@ -8,9 +8,7 @@ import 'package:quizzApp/presentation/presenters/presenters.dart';
 import 'package:quizzApp/presentation/protocols/protocols.dart';
 
 class ValidationSpy extends Mock implements Validation {}
-
 class AuthenticationSpy extends Mock implements Authentication {}
-
 class SaveCurrentAccountSpy extends Mock implements SaveCurrentAccount {}
 
 void main() {
@@ -159,7 +157,7 @@ void main() {
     sut.validateEmail(email);
     sut.validatePassword(password);
 
-    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    expectLater(sut.isLoadingStream, emits(true));
 
     await sut.auth();
   });
