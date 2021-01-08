0cef2658caecf20d9caaf0bbd347850a06998e67
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2021-01-08 17:35:16 -0300

    ensure GetxLoginPresenter emits error if SaveCurrentAccount fails

--- test/presentation/presenters/getx_login_presenter_test.dart
+++ test/presentation/presenters/getx_login_presenter_test.dart
@@ -29,6 +29,8 @@ void main() {
 
   PostExpectation mockAuthenticationCall() => when(authentication.auth(any));
 
+  PostExpectation mockSaveCurrentAccountCall() => when(saveCurrentAccount.save(any));
+
   void mockValidation({String field, String value}) {
     mockValidationCall(field).thenReturn(value);
   }
@@ -41,6 +43,10 @@ void main() {
 
   void mockAuthenticationError(DomainError error) {
     mockAuthenticationCall().thenThrow(error);
+  }
+
+  void mockSaveCurrentAccountError() {
+    mockSaveCurrentAccountCall().thenThrow(DomainError.unexpected);
   }
 
   setUp(() {
@@ -191,5 +197,18 @@ void main() {
     await sut.auth();
 
     verify(saveCurrentAccount.save(AccountEntity(token))).called(1);
+  });
+
+  test('Should emit UnexpectedError if SaveCurrentAccount fails', () async {
+    mockSaveCurrentAccountError();
+
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    sut.mainErrorStream.listen(expectAsync1((error) => expect(error, 'Algo inesperado aconteceu! Tente novamente em breve.')));
+
+    await sut.auth();
   });
 }
