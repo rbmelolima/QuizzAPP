91902694431f8f0b704dd433618df9efca549658
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2021-01-18 18:50:27 -0300

    move GetxSplashPresenter to production code

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,29 +1,9 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/get.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:quizzApp/domain/entities/account_entity.dart';
 import 'package:quizzApp/domain/usecases/usecases.dart';
-import 'package:quizzApp/ui/pages/pages.dart';
-
-class GetxSplashPresenter implements SplashPresenter {
-  final LoadCurrentAccount loadCurrentAccount;
-  var _navigateTo = RxString();
-
-  GetxSplashPresenter({@required this.loadCurrentAccount});
-
-  Stream<String> get navigateToStream => _navigateTo.stream;
-
-  Future<void> checkAccount() async {
-    try {
-      final account = await loadCurrentAccount.load();
-      _navigateTo.value = account.isNull ? '/login' : '/surveys';
-    } catch (error) {
-      _navigateTo.value = '/login';
-    }
-  }
-}
+import 'package:quizzApp/presentation/presenters/presenters.dart';
 
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 
