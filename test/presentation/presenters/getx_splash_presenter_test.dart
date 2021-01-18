195fbc8026405cf3087be6bdef80eaca6b769115
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2021-01-18 18:39:42 -0300

    ensure GetxSplashPresenter emits surveys page on success

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -15,6 +15,7 @@ class GetxSplashPresenter implements SplashPresenter {
 
   Future<void> checkAccount() async {
     await loadCurrentAccount.load();
+    _navigateTo.value = '/surveys';
   }
 }
 
@@ -32,5 +33,13 @@ void main() {
   test('Should call LoadCurrentAccount', () async {
     await sut.checkAccount();
     verify(loadCurrentAccount.load()).called(1);
+  });
+
+  test('Should go to surveys page on success', () async {
+    sut.navigateToStream.listen(
+      expectAsync1((page) => expect(page, '/surveys')),
+    );
+
+    await sut.checkAccount();
   });
 }
